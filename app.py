a8b3c1c788d5f715cbb7583a8ca6524ea004d4fe
Sitou Miguel Efraim Agbodjinou <mltd1985@hotmail.com>
2025-06-10 17:17:47 +0000

    🎯 Containerized Flask app with Kubernetes deployment on GKE

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return jsonify({"message": "Hello from Cloud Run!"})
+    return "Hello from Kubernetes!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+
